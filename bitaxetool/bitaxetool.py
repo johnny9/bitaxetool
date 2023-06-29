f79a43a718ff0133714a54912f9d572e1eff207f
johnny9 <985648+johnny9@users.noreply.github.com>
2023-06-29 11:22:37 -0400

    main: add initial command for flashing bitaxe

---
+++ bitaxetool/bitaxetool.py
@@ -0,0 +1,49 @@
+import argparse
+import nvs_partition_gen
+import esptool
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Flash firmware and config to a device.')
+
+    parser.add_argument('--port', 
+                        type=str, 
+                        default=None, 
+                        help='The serial port to connect to.')
+    parser.add_argument('--firmware', 
+                        type=str, 
+                        required=True, 
+                        help='The binary file to flash.')
+    parser.add_argument('--config', 
+                        type=str, 
+                        required=True, 
+                        help='The config file to flash.')
+
+    return parser.parse_args()
+
+
+def flash_bitaxe(firmware_path, config_path, serial_port):
+    nvs_gen_args = argparse.Namespace(input=config_path,
+                                      output='config.bin',
+                                      outdir='/tmp',
+                                      size='0x6000',
+                                      version=2)
+    nvs_partition_gen.generate(nvs_gen_args)
+
+    esp_arguments = []
+    if serial_port is not None:
+        esp_arguments += ['--port', serial_port]
+    esp_arguments += ['write_flash', '0x9000', '/tmp/config.bin', '0x10000', firmware_path]
+    esptool.main(esp_arguments)
+
+
+def main():
+    args = parse_args()
+    if args.port is not None:
+        print(f"Connecting to port: {args.port}")
+    print(f"Flashing firmware: {args.firmware}")
+    print(f"Flashing config: {args.config}")
+    flash_bitaxe(args.firmware, args.config, args.port)
+
+
+if __name__ == "__main__":
+    main()
